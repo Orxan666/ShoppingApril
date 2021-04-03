5332bc7b0286408eee1b7c2746d540fcc2fd6eac
Orxan666 <orxanallahyarov03@gmail.com>
2021-04-03 13:26:14 +0400

    Add project files.

---
+++ ShoppingApril/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace ShoppingApril
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pcWelcome = new System.Windows.Forms.PictureBox();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcWelcome)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcWelcome
+            // 
+            this.pcWelcome.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pcWelcome.Image = ((System.Drawing.Image)(resources.GetObject("pcWelcome.Image")));
+            this.pcWelcome.Location = new System.Drawing.Point(176, 74);
+            this.pcWelcome.Name = "pcWelcome";
+            this.pcWelcome.Size = new System.Drawing.Size(467, 280);
+            this.pcWelcome.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcWelcome.TabIndex = 0;
+            this.pcWelcome.TabStop = false;
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblWelcome.Location = new System.Drawing.Point(345, 9);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(124, 42);
+            this.lblWelcome.TabIndex = 1;
+            this.lblWelcome.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(158, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Register";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(514, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 52);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.pcWelcome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcWelcome)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcWelcome;
+        private System.Windows.Forms.Label lblWelcome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
